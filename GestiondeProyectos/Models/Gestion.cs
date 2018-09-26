6a1eb3b4c642f6d935e835a72b26d0fae289a43b
mariana <mariana@LAPTOP-GH4ATAOK>
2018-09-26 18:59:26 -0500

    mi primera version de codigo

---
+++ GestiondeProyectos/Models/Gestion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GestiondeProyectos.Models
+{
+    public class Gestion
+    {
+        public int ID;
+        public string Name;
+        public string Last_Name;
+        public string Name_Project;
+        public DateTime Dtae;
+
+    }
+}
---
+++ GestiondeProyectos/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GestiondeProyectos.Startup))]
+namespace GestiondeProyectos
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
